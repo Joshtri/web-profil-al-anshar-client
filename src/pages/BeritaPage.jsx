ba89dc5e72e5a91f3fbeea15b250e96caab242c8
Joshtri <stuffofyos1516@gmail.com>
2024-11-22 13:20:23 +0800

    updating berita page scroll

--- src/pages/BeritaPage.jsx
+++ src/pages/BeritaPage.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from './Layout'
 import BeritaSection from '../components/Berita/BeritaSection'
 
 function BeritaPage() {
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []); // The empty dependency array ensures this runs only once when the component mounts
+
   return (
     <Layout>
         <BeritaSection/>
